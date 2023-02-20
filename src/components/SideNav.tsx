8a3014d73920467eab3c674a8f5d0a27464f3992
Agung Cahya <agungcahya122@gmail.com>
2023-02-20 22:59:14 +0700

    fix: bug navigate sideBar

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -37,7 +37,7 @@ export default function SideNav() {
               />
             </li>
 
-            <li className="mt-4 ml-5">
+            <li className="mt-4 ml-5" onClick={() => navigate("/listProduct")}>
               <CustomButton
                 id="btn-produk"
                 icon={<FaBoxOpen className="w-[1.8rem] h-[1.8rem] mr-5" />}
@@ -45,7 +45,7 @@ export default function SideNav() {
                 className="text-2xl text-orangeComponent font-poppins font-semibold ml-3 hover:bg-orangeComponent py-2 p-4 w-[80%] hover:text-white flex flex-row hover:rounded-xl"
               />
             </li>
-            <li className="mt-4 ml-5">
+            <li className="mt-4 ml-5" onClick={() => navigate("/listMember")}>
               <Link to="/listMember">
                 <CustomButton
                   id="btn-member"
@@ -57,7 +57,7 @@ export default function SideNav() {
                 />
               </Link>
             </li>
-            <li className="mt-4 ml-5">
+            <li className="mt-4 ml-5" onClick={() => navigate("/report")}>
               <CustomButton
                 id="btn-laporan"
                 icon={<FaBookReader className="w-[1.8rem] h-[1.8rem] mr-5" />}
